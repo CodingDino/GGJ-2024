87f541099dfa6fd2bf185d9e8bf96c2243c90853
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2024-01-27 13:34:07 +0000

    menu sort of set

---
+++ mainMenu.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class mainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
+
+private void _StartGame()
+{
+	get_tree().change_scene();
+}
+
+private void _ApplicationQuit()
+{
+	get_tree().quit(); 
+}
