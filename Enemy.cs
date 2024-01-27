8fa5b14c8f22fffd6a8071e821f0066f6691a64f
Dust-Green <44965194+Dust-Green@users.noreply.github.com>
2024-01-27 20:06:50 +0000

    Set up state skeleton
    Now the enemy will only follow if it's close enough.  if you leave it's zone it stops following. Set up some comments for where I think the animations and audio players should go.

--- Enemy.cs
+++ Enemy.cs
@@ -8,24 +8,69 @@ public partial class Enemy : CharacterBody3D
 
     [Export]
     NodePath playerPath;
-    CharacterBody3D player;
+    Godot.CharacterBody3D player;
     NavigationAgent3D navAgent;
-    const float speed = 2.6969696969f;
+    const float speed = 2.420f;
+    Godot.Vector3 velocity;
+    float followRange = 5f;
+    float attackRange = 1f;
 
     public override void _Ready() 
     {
-        player = GetNode<CharacterBody3D>(playerPath);
+        player = GetNode<Godot.CharacterBody3D>(playerPath); //../Player
         navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+        
     }
 
     public override void _PhysicsProcess(double delta)
     {
         Velocity = Godot.Vector3.Zero;
         navAgent.TargetPosition = player.GlobalTransform.Origin;
-        Godot.Vector3 nextNavPoint  = navAgent.GetNextPathPosition();
-        Velocity = (nextNavPoint - GlobalTransform.Origin).Normalized() * speed;
+        navAgent.TargetDesiredDistance = 1.0f;
         Godot.Vector3 lookatTarget = new Godot.Vector3(player.GlobalPosition.X,GlobalPosition.Y,player.GlobalPosition.Z);
+        if(playerInRangeToFollow())
+        {
+            //Play walking animation
+            Godot.Vector3 nextNavPoint  = navAgent.GetNextPathPosition();
+            velocity = (nextNavPoint - GlobalTransform.Origin).Normalized() * speed;
+            if(playerInRangeToAttack())
+            {
+                //Attack Goes here!
+                //Play Attack Animation
+                //play big laugh audio
+                //Send Damage to Player
+            }
+                
+            
+        }
+        else
+        {
+            //Play idle animation
+            //Set audio streamer to 'idle' laugh
+            velocity = Godot.Vector3.Zero;
+        }
+
+        
+        
+        
+
+        
+        
         LookAt(lookatTarget);
+        Velocity = velocity;
         MoveAndSlide();
     }
+
+    public bool playerInRangeToFollow()
+    {
+        return GlobalPosition.DistanceTo(player.GlobalPosition) < followRange;
+    }
+    public bool playerInRangeToAttack()
+    {
+        return GlobalPosition.DistanceTo(player.GlobalPosition) < attackRange;
+    }
+
+    
+
+    
 }
